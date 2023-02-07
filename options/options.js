21137adeea84fbe7ded99c258f31764b1164c415
Crowdbotics <team@crowdbotics.com>
2023-02-07 12:59:51 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "athletedirector-38768",
+  url: "https://athletedirector-38768.botics.co",
+  api: "https://athletedirector-38768.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
